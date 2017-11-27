9ecaa6f7d9a4060a42aa682c0ab3f4c3c5e07d0e
Matt McShane <matt@mattmcshane.com>
2017-11-26 21:05:55 -0500

    salpdemo clean shutdown from SIGINT
    The salpdemo program will now properly shut down its probe provider when
    the process is killed via SIGINT. This means that it won't leave
    orphaned .so files under /tmp anymore.

--- cmd/demo/salpdemo.go
+++ cmd/demo/salpdemo.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/mmcshane/salp"
@@ -14,16 +17,30 @@ var (
 )
 
 func main() {
+	fmt.Println("List the go probes in this demo with")
+	fmt.Println("\tsudo tplist -vp \"$(pgrep salpdemo)\" \"salpdemo*\"")
+	fmt.Println("Trace this process with")
+	fmt.Println("\tsudo trace -p \"$(pgrep salpdemo)\" 'u::p1 \"arg1=%d arg2=%s\", arg1, arg2' 'u::p2 \"arg1=%d\", arg1'")
+
 	probes.Load()
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
 	var i, j int8
+
+loop:
 	for {
-		s := time.Now().Format(time.RFC850)
-		p1.Fire(i, s)
-		p2.Fire(j, s)
-		i += 1
-		j += 2
-		time.Sleep(1 * time.Second)
+		select {
+		case <-c:
+			break loop
+		case <-time.After(1 * time.Second):
+			s := time.Now().Format(time.RFC850)
+			p1.Fire(i, s)
+			p2.Fire(j, s)
+			i += 1
+			j += 2
+		}
 	}
 	probes.Unload()
 	probes.Dispose()
