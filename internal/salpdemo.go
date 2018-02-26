dc2586ca861adaf32f9bb2a144982057976cf1ed
Matt McShane <matt@mattmcshane.com>
2018-02-26 10:31:31 -0500

    Improvements to demo
    - Use new salp.Bool argument type
    - Include an example that uses filtering

--- internal/salpdemo.go
+++ internal/salpdemo.go
@@ -12,8 +12,8 @@ import (
 var (
 	probes = salp.MakeProvider("salp-demo")
 
-	p1 = salp.MustAddProbe(probes, "p1", salp.Int8, salp.String)
-	p2 = salp.MustAddProbe(probes, "p2", salp.Uint8, salp.String)
+	p1 = salp.MustAddProbe(probes, "p1", salp.Int32, salp.Error, salp.String)
+	p2 = salp.MustAddProbe(probes, "p2", salp.Uint8, salp.Bool)
 )
 
 func main() {
@@ -21,7 +21,9 @@ func main() {
 	fmt.Println("List the go probes in this demo with")
 	fmt.Println("\tsudo tplist -vp \"$(pgrep salpdemo)\" \"salp-demo*\"")
 	fmt.Println("Trace this process with")
-	fmt.Println("\tsudo trace -p \"$(pgrep salpdemo | head -n1)\" 'u::p1 \"arg1=%d arg2=%s\", arg1, arg2' 'u::p2 \"arg1=%d\", arg1'")
+	fmt.Println("\tsudo trace -p \"$(pgrep salpdemo | head -n1)\" 'u::p1 \"i=%d err=`%s` date=`%s`\", arg1, arg2, arg3' 'u::p2 \"j=%d flag=%d\", arg1, arg2'")
+	fmt.Println("\tor")
+	fmt.Println("\tsudo trace -p \"$(pgrep salpdemo | head -n1)\" 'u::p1 (arg1 % 2 == 0) \"i=%d err='%s'\", arg1, arg2'")
 
 	probes.Load()
 
@@ -36,8 +38,8 @@ func main() {
 			return
 		case now := <-time.After(1 * time.Second):
 			s := now.Format(time.RFC1123Z)
-			p1.Fire(i, s)
-			p2.Fire(j, s)
+			p1.Fire(i, fmt.Errorf("An error: %d", i), s)
+			p2.Fire(j, j%4 == 0)
 			i += 1
 			j += 2
 		}
